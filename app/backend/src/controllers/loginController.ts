6176935a9374d60d36bcdaf7d143ac4dc2300907
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-29 11:10:04 -0300

    feat: implementando controller do login

---
+++ app/backend/src/controllers/loginController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import LoginServices from '../services/loginServices';
+
+class LoginController {
+  static async login(req: Request, res: Response) {
+    try {
+      const { email } = req.body;
+      const user = await LoginServices.create(email);
+      return res.status(200).json(user);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export default LoginController;
