fba598aed86bc573b1a74354d1fc8742df7775fa
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-29 11:18:15 -0300

    feat: implmentando o services do login

---
+++ app/backend/src/services/loginServices.ts
@@ -0,0 +1,22 @@
+import UserModel from '../database/models/User';
+import tokenJwt from '../utils/tokenJwt';
+import Iuser from '../interface/user';
+
+class LoginServices {
+  static async create(email: string) {
+    const user = await UserModel.findOne({
+      where: {
+        email,
+      },
+    }) as Iuser || null;
+
+    const token = await tokenJwt.createToken({ id: user.id, username: user.username });
+    return {
+      user:
+      { id: user.id, username: user.username, role: user.role, email: user.email },
+      token,
+    };
+  }
+}
+
+export default LoginServices;
