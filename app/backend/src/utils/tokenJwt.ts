7a292592b9ac2dff5776a177095a513f5972cd42
Wilian Pereira <wilian_pereira92@outlook.com>
2022-04-29 11:20:04 -0300

    feat: implementando as funções criar token e validar

---
+++ app/backend/src/utils/tokenJwt.ts
@@ -0,0 +1,37 @@
+import * as jwt from 'jsonwebtoken';
+import { readFile } from 'fs/promises';
+
+const getSecret = async () => readFile('jwt.evaluation.key', 'utf-8');
+
+const createToken = async (
+  payload: {
+    id: number,
+    username: string,
+  },
+) => {
+  const jwtConfig = {
+    expiresIn: '5d',
+  };
+
+  const secret = await getSecret();
+
+  const token = jwt.sign(payload, secret, jwtConfig);
+
+  return token;
+};
+
+const validateToken = async (token: string) => {
+  try {
+    const secret = await getSecret();
+
+    const decoded = jwt.verify(token, secret);
+    return decoded;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  createToken,
+  validateToken,
+};
