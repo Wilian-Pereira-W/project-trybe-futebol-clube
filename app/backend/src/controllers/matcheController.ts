1083458c03be01fb1be743ffdec816558ee676e4
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-03 13:03:36 -0300

    feat: pegando a query inProgress

--- app/backend/src/controllers/matcheController.ts
+++ app/backend/src/controllers/matcheController.ts
@@ -3,8 +3,9 @@ import MatchServices from '../services/matchServices';
 
 class MatcheController {
   static async findAllMatches(req: Request, res: Response){
+    const {inProgress} = req.query;
     try {
-      const matches = await MatchServices.findAllMatches();
+      const matches = await MatchServices.findAllMatches(inProgress);
       return res.status(200).json(matches);
     } catch (error) {
       console.log(error);
