2caed14b3f2983e584b023c419b3d56436a159d3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-03 17:49:16 -0300

    refactor: Indentação das linhas

--- app/backend/src/controllers/matcheController.ts
+++ app/backend/src/controllers/matcheController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import MatchServices from '../services/matchServices';
 
 class MatcheController {
-  static async findAllMatches(req: Request, res: Response){
-    const {inProgress} = req.query;
+  static async findAllMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
     try {
       const matches = await MatchServices.findAllMatches(inProgress);
       return res.status(200).json(matches);
@@ -13,19 +13,21 @@ class MatcheController {
   }
 
   static async createMatches(req: Request, res: Response) {
-    const {homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress} = req.body;
+    const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress } = req.body;
     try {
-      if(homeTeam === awayTeam) {
-        return res.status(401).json({ message: 'It is not possible to create a match with two equal teams'} );
+      if (homeTeam === awayTeam) {
+        return res.status(401)
+          .json({ message: 'It is not possible to create a match with two equal teams' });
       }
       const thereIsTeam = await MatchServices.thereIsTheTeam(homeTeam, awayTeam);
-      if(thereIsTeam) {
-        return res.status(404).json({ message: 'There is no team with such id!'} );
+      if (thereIsTeam) {
+        return res.status(404).json({ message: 'There is no team with such id!' });
       }
-      const newMatch = await MatchServices.createMatches(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress);
+      const newMatch = await MatchServices
+        .createMatches(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress);
       return res.status(201).json(newMatch);
     } catch (error) {
-      
+      console.log(error);
     }
   }
 }
--- app/backend/src/services/matchServices.ts
+++ app/backend/src/services/matchServices.ts
@@ -1,46 +1,41 @@
-import TeamModel from "../database/models/Team";
-import MatchModel from "../database/models/Matche";
-import { where } from "sequelize/types";
+import TeamModel from '../database/models/Team';
+import MatchModel from '../database/models/Matche';
 
 class MatchServices {
   static async findAllMatches(inProgress: any) {
-    if(inProgress === undefined){
+    if (inProgress === undefined) {
       const matches = await MatchModel.findAll({
         include: [
-          {model: TeamModel, as: 'teamHome', attributes: {exclude: ['id']}},
-          {model: TeamModel, as: 'teamAway', attributes: {exclude: ['id']}}
-        ]
-      });
-      return matches;
-    }
-    if(inProgress === 'false' || inProgress === 'true') {
-      const boolean = inProgress === "true";
-      const matches = await MatchModel.findAll({
-        include: [
-          {model: TeamModel, as: 'teamHome', attributes: {exclude: ['id']}},
-          {model: TeamModel, as: 'teamAway', attributes: {exclude: ['id']}}
+          { model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+          { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
         ],
-        where: {
-          inProgress: boolean,
-        }
       });
       return matches;
     }
-   
+    if (inProgress === 'false' || inProgress === 'true') {
+      const boolean = inProgress === 'true';
+      const matches = await MatchModel.findAll({
+        include: [{ model: TeamModel, as: 'teamHome', attributes: { exclude: ['id'] } },
+          { model: TeamModel, as: 'teamAway', attributes: { exclude: ['id'] } },
+        ],
+        where: { inProgress: boolean } });
+      return matches;
+    }
   }
 
   static async createMatches(homeTeam: number, awayTeam: number, homeTeamGoals: number, awayTeamGoals: number, inProgress: boolean) {
-    const newMatch = await MatchModel.create({homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress});
+    const newMatch = await MatchModel
+      .create({ homeTeam, homeTeamGoals, awayTeam, awayTeamGoals, inProgress });
     return newMatch;
   }
 
   static async thereIsTheTeam(homeTeam: number, awayTeam: number) {
     const thereIsHomeTeam = await TeamModel.findByPk(homeTeam);
     const thereIsAwayTeam = await TeamModel.findByPk(awayTeam);
-    if( thereIsHomeTeam === null || thereIsAwayTeam === null) {
+    if (thereIsHomeTeam === null || thereIsAwayTeam === null) {
       return true;
     }
   }
-};
+}
 
 export default MatchServices;
