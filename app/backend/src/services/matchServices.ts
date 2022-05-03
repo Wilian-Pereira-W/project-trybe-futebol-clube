044842ad83bc8d5ca3a1db4ce94b757de3cbfa04
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-03 13:04:27 -0300

    feat: implementando os filtros pelo inProgress

--- app/backend/src/services/matchServices.ts
+++ app/backend/src/services/matchServices.ts
@@ -1,16 +1,32 @@
 import TeamModel from "../database/models/Team";
 import MatchModel from "../database/models/Matche";
+import { where } from "sequelize/types";
 
 class MatchServices {
-  static async findAllMatches() {
-    const matches = await MatchModel.findAll({
-      include: [
-        {model: TeamModel, as: 'teamHome'},
-        {model: TeamModel, as: 'teamAway'}
-      ]
-    });
-    console.log('matchesss', matches);
-    return matches;
+  static async findAllMatches(inProgress: any) {
+    if(inProgress === undefined){
+      const matches = await MatchModel.findAll({
+        include: [
+          {model: TeamModel, as: 'teamHome', attributes: {exclude: ['id']}},
+          {model: TeamModel, as: 'teamAway', attributes: {exclude: ['id']}}
+        ]
+      });
+      return matches;
+    }
+    if(inProgress === 'false' || inProgress === 'true') {
+      const boolean = inProgress === "true";
+      const matches = await MatchModel.findAll({
+        include: [
+          {model: TeamModel, as: 'teamHome', attributes: {exclude: ['id']}},
+          {model: TeamModel, as: 'teamAway', attributes: {exclude: ['id']}}
+        ],
+        where: {
+          inProgress: boolean,
+        }
+      });
+      return matches;
+    }
+   
   }
 };
 
