86c91a999e83227a2045af2d0dd6bbe728da159c
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-03 15:24:14 -0300

    feat: implementando o controller do thereIsTheTeam

--- app/backend/src/controllers/matcheController.ts
+++ app/backend/src/controllers/matcheController.ts
@@ -15,6 +15,13 @@ class MatcheController {
   static async createMatches(req: Request, res: Response) {
     const {homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress} = req.body;
     try {
+      if(homeTeam === awayTeam) {
+        return res.status(401).json({ message: 'It is not possible to create a match with two equal teams'} );
+      }
+      const thereIsTeam = await MatchServices.thereIsTheTeam(homeTeam, awayTeam);
+      if(thereIsTeam) {
+        return res.status(404).json({ message: 'There is no team with such id!'} );
+      }
       const newMatch = await MatchServices.createMatches(homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress);
       return res.status(201).json(newMatch);
     } catch (error) {
