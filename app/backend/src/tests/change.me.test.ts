cf2328ccb4edf89448fced9c3f79a6d4b37b7fc3
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-04 12:43:50 -0300

    feat: implementando os testes da rota post  /login

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -1,44 +1,88 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
 import chaiHttp = require('chai-http');
+import * as jwt from 'jsonwebtoken';
 
 import { app } from '../app';
-import Example from '../database/models/ExampleModel';
 
 import { Response } from 'superagent';
+import  User from '../database/models/User';
+import userMock from './mock/user'
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
-  /**
-   * Exemplo do uso de stubs com tipos
-   */
 
-  // let chaiHttpResponse: Response;
+describe('Post /login', () => {
 
-  // before(async () => {
-  //   sinon
-  //     .stub(Example, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as Example);
-  // });
-
-  // after(()=>{
-  //   (Example.findOne as sinon.SinonStub).restore();
-  // })
-
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  before(async () => {
+    sinon
+      .stub(User, "findOne")
+      .resolves(userMock as User);
   });
+
+  after(()=>{
+    (User.findOne as sinon.SinonStub).restore();
+  })
+  describe('Quando não é passado o email e a senha', () => {
+    let response: Response;
+    before( async () => {
+      response = await chai.request(app).post('/login').send({});
+    });
+    it('retorna código de status 400', () => {
+      expect(response).to.have.status(400);
+    });
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
+    it('objeto de resposta possui a proprieade "message"', () => {
+      expect(response.body.message).to.be.equals('All fields must be filled');
+    })
+  });
+  describe('Quando passado um email ou password incorreto', () => {
+    let response: Response;
+    before( async () => {
+      response = await chai.request(app).post('/login').send({email: 'test.test.com', password: 1234});
+    });
+    it('retorna código de status 401', () => {
+      expect(response).to.have.status(401);
+    });
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
+    it('objeto de resposta possui a proprieade "message"', () => {
+      expect(response.body.message).to.be.equals('Incorrect email or password');
+    })
+  });
+
+  describe('Quando login é feito com sucesso', () => {
+    let response: Response;
+    before( async () => {
+      response = await chai.request(app).post('/login').send({ email: 'admin@admin.com',
+      password: 'secret_admin', });
+    });
+    it('retorna código de status 200', () => {
+      expect(response).to.have.status(200);
+    });
+    it('retorna um objeto no body', () => {
+      expect(response.body).to.be.an('object');
+    });
+    it('objeto de resposta possui a propriedade "user"', () => {
+      expect(response.body.user.username).to.be.equals('Admin');
+      expect(response.body.user.role).to.be.equals('admin');
+      expect(response.body.user.email).to.be.equals('admin@admin.com');
+    });
+    // it('objeto de resposta possui a propriedade "token"', () => {
+    //   const { token } = response.body;
+    //   const payload = jwt.decode(token);
+    //   expect(payload).to.be.equals('admin')
+    //   console.log(payload);
+    // });
+    it('objeto de resposta possui a propriedade "token"', () => {
+      expect(response.body).to.be.property('token');
+    });
+  });
+
+
 });
