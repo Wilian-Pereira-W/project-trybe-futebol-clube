5e79a49e03ad33b2496e8cc963605090dacbec6d
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:49:11 -0300

    fix: trocando map por forEach na função goalsFavor

--- app/backend/src/utils/goalsFavor.ts
+++ app/backend/src/utils/goalsFavor.ts
@@ -1,10 +1,9 @@
-import IhomeMatche from "../interface/homeMatche";
+import IhomeMatche from '../interface/homeMatche';
+
 const goalsFavor = (homeMatche: IhomeMatche[]) => {
   let golFeitos = 0;
-  homeMatche.map((gol) => {
-      golFeitos += gol.homeTeamGoals 
-  });
+  homeMatche.forEach((gol) => { golFeitos += gol.homeTeamGoals; });
   return golFeitos;
-}
+};
 
 export default goalsFavor;
