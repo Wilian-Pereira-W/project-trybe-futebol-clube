ad3ee8f3dcd3b2518f38c6bfa5c5544b44b3d742
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:46:16 -0300

    style: indentação na interface IhomeResults

--- app/backend/src/interface/homeResults.ts
+++ app/backend/src/interface/homeResults.ts
@@ -1,14 +1,14 @@
 interface IhomeResults {
-    name: string,
-    totalPoints: number,
-    totalGames: number,
-    totalVictories: number,
-    totalDraws: number,
-    totalLosses: number,
-    goalsFavor: number,
-    goalsOwn: number,
-    goalsBalance: number,
-    efficiency: number
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+  goalsBalance: number,
+  efficiency: number
 }
 
 export default IhomeResults;
