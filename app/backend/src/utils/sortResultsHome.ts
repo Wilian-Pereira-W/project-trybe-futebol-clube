91f066f5e9d861a95aea77f1ff0635d603578253
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-06 15:20:21 -0300

    fix: alterando os valores do return

--- app/backend/src/utils/sortResultsHome.ts
+++ app/backend/src/utils/sortResultsHome.ts
@@ -3,12 +3,12 @@ import IhomeResults from '../interface/homeResults';
 const sortResultsHome = (a: IhomeResults, b: IhomeResults) => {
   if (b.totalPoints > a.totalPoints) { return 1; }
   if (b.totalPoints < a.totalPoints) { return -1; }
-  if (b.goalsBalance > a.goalsBalance) { return 0.5; }
-  if (b.goalsBalance < a.goalsBalance) { return -0.5; }
-  if (b.goalsFavor > a.goalsFavor) { return 0.3; }
-  if (b.goalsFavor < a.goalsFavor) { return -0.3; }
-  if (b.goalsOwn > a.goalsOwn) { return 0.2; }
-  if (b.goalsOwn < a.goalsOwn) { return -0.2; }
+  if (b.goalsBalance > a.goalsBalance) { return 1; }
+  if (b.goalsBalance < a.goalsBalance) { return -1; }
+  if (b.goalsFavor > a.goalsFavor) { return 1; }
+  if (b.goalsFavor < a.goalsFavor) { return -1; }
+  if (b.goalsOwn > a.goalsOwn) { return 1; }
+  if (b.goalsOwn < a.goalsOwn) { return -1; }
   return 0;
 };
 
