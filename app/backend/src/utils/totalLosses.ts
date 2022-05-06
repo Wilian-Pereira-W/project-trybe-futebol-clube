5d339ef61404a8bb2a698fdc7cde04ebc3bea66f
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 22:46:01 -0300

    feat: implementado a função totalLosses

---
+++ app/backend/src/utils/totalLosses.ts
@@ -0,0 +1,12 @@
+import IhomeMatche from "../interface/homeMatche";
+const totalLosses = (homeMatche: IhomeMatche[]) => {
+  let derrota = 0;
+  homeMatche.map((gol) => {
+    if(gol.homeTeamGoals < gol.awayTeamGoals) {
+      derrota += 1;
+    }
+  });
+  return derrota;
+}
+
+export default totalLosses;
