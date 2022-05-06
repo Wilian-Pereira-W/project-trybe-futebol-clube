e0d47be3010d500bef254473cb7771d0a72cb31e
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:51:44 -0300

    fix: trocando map por forEach na função totalPoints

--- app/backend/src/utils/totalPoints.ts
+++ app/backend/src/utils/totalPoints.ts
@@ -1,16 +1,17 @@
-import IhomeMatche from "../interface/homeMatche";
+import IhomeMatche from '../interface/homeMatche';
+
 const totalPoints = (homeMatche: IhomeMatche[]) => {
   let pontos = 0;
-  homeMatche.map((gol) => {
-    if(gol.homeTeamGoals > gol.awayTeamGoals) {
+  homeMatche.forEach((gol) => {
+    if (gol.homeTeamGoals > gol.awayTeamGoals) {
       pontos += 3;
-    } else if(gol.homeTeamGoals === gol.awayTeamGoals){
+    } else if (gol.homeTeamGoals === gol.awayTeamGoals) {
       pontos += 1;
     } else {
       pontos += 0;
     }
   });
   return pontos;
-}
+};
 
 export default totalPoints;
