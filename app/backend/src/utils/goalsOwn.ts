39066c2c2b9b9d2f892694ff62f27ab4a309178b
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:49:27 -0300

    fix: trocando map por forEach na função goalsOwn

--- app/backend/src/utils/goalsOwn.ts
+++ app/backend/src/utils/goalsOwn.ts
@@ -1,10 +1,11 @@
-import IhomeMatche from "../interface/homeMatche";
+import IhomeMatche from '../interface/homeMatche';
+
 const goalsOwn = (homeMatche: IhomeMatche[]) => {
   let golSofrido = 0;
-  homeMatche.map((gol) => {
-    golSofrido += gol.awayTeamGoals
+  homeMatche.forEach((gol) => {
+    golSofrido += gol.awayTeamGoals;
   });
   return golSofrido;
-}
+};
 
 export default goalsOwn;
