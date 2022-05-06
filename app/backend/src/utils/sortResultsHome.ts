375ad9eafc5bbcef02615994017838a4f6de7f17
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:50:07 -0300

    style: indentação na função sortResultsHome

--- app/backend/src/utils/sortResultsHome.ts
+++ app/backend/src/utils/sortResultsHome.ts
@@ -1,32 +1,15 @@
-import IhomeResults from "../interface/homeResults";
+import IhomeResults from '../interface/homeResults';
 
-const sortResultsHome = (a: IhomeResults, b: IhomeResults ) => {
-
-  if(b.totalPoints > a.totalPoints){
-    return 1
-  }
-  if(b.totalPoints < a.totalPoints){
-    return -1
-  }
-  if(b.goalsBalance > a.goalsBalance){
-    return 0.5
-  }
-  if(b.goalsBalance < a.goalsBalance){
-    return -0.5
-  }
-  if(b.goalsFavor > a.goalsFavor){
-    return 0.3
-  }
-  if(b.goalsFavor < a.goalsFavor){
-    return -0.3
-  }
-  if(b.goalsOwn > a.goalsOwn){
-    return 0.2
-  }
-  if(b.goalsOwn < a.goalsOwn){
-    return -0.2
-  }
-  return 0
-}
+const sortResultsHome = (a: IhomeResults, b: IhomeResults) => {
+  if (b.totalPoints > a.totalPoints) { return 1; }
+  if (b.totalPoints < a.totalPoints) { return -1; }
+  if (b.goalsBalance > a.goalsBalance) { return 0.5; }
+  if (b.goalsBalance < a.goalsBalance) { return -0.5; }
+  if (b.goalsFavor > a.goalsFavor) { return 0.3; }
+  if (b.goalsFavor < a.goalsFavor) { return -0.3; }
+  if (b.goalsOwn > a.goalsOwn) { return 0.2; }
+  if (b.goalsOwn < a.goalsOwn) { return -0.2; }
+  return 0;
+};
 
 export default sortResultsHome;
