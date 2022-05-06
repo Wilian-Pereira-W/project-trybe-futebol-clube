7c1969669ecc568809bc264c2f148b3d090daa78
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:52:00 -0300

    fix: trocando map por forEach na função totalVictories

--- app/backend/src/utils/totalVictories.ts
+++ app/backend/src/utils/totalVictories.ts
@@ -1,12 +1,13 @@
-import IhomeMatche from "../interface/homeMatche";
+import IhomeMatche from '../interface/homeMatche';
+
 const totalVictories = (homeMatche: IhomeMatche[]) => {
   let vitoria = 0;
-  homeMatche.map((gol) => {
-    if(gol.homeTeamGoals > gol.awayTeamGoals) {
+  homeMatche.forEach((gol) => {
+    if (gol.homeTeamGoals > gol.awayTeamGoals) {
       vitoria += 1;
     }
   });
   return vitoria;
-}
+};
 
 export default totalVictories;
