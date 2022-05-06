cb7b9da8e8596f2a32adaee9c24628e00f6a0790
Wilian Pereira <wilian_pereira92@outlook.com>
2022-05-05 23:51:14 -0300

    fix: trocando map por forEach na função totalDraws

--- app/backend/src/utils/totalDraws.ts
+++ app/backend/src/utils/totalDraws.ts
@@ -1,12 +1,13 @@
-import IhomeMatche from "../interface/homeMatche";
+import IhomeMatche from '../interface/homeMatche';
+
 const totalDraws = (homeMatche: IhomeMatche[]) => {
   let empate = 0;
-  homeMatche.map((gol) => {
-    if(gol.homeTeamGoals === gol.awayTeamGoals) {
+  homeMatche.forEach((gol) => {
+    if (gol.homeTeamGoals === gol.awayTeamGoals) {
       empate += 1;
     }
   });
   return empate;
-}
+};
 
 export default totalDraws;
